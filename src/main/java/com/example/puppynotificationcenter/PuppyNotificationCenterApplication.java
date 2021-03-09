666124dd8506fea19936cdac8ded10e79ebf0287
U-DESKTOP-TM45V7L\corn_ <corn_brandon@columbusstate.edu>
2021-03-09 13:36:25 -0500

    project skeleton

---
+++ src/main/java/com/example/puppynotificationcenter/PuppyNotificationCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.puppynotificationcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyNotificationCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyNotificationCenterApplication.class, args);
+	}
+
+}
